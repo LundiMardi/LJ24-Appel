03c71dda7c6fb5c68cc703d83fd7b2802102b66e
Gael <gael.paccard@heig-vd.ch>
2022-07-23 15:39:59 +0200

    mobile menu transition added

--- source/javascript/mobileMenu.js
+++ source/javascript/mobileMenu.js
@@ -1,5 +1,29 @@
 const mobileMenu = function() {
-  console.log('mobilemenu');
 
-  const navButtonOpen = document.getElementById('bt-mobile-menu) ;
+  const navButtonOpen = document.getElementById('bt-mobile-menu') ;
+  const navButtonHide = document.getElementById('bt-mobile-menu-hide');
+  const menuElement = document.getElementById('mobile-menu');
+
+  // openning the menu
+  navButtonOpen.addEventListener('click', event => {
+    menuElement.classList.remove('mobile-menu__is-closed');
+    navButtonOpen.classList.remove('mobile-menu__is-closed');
+    navButtonHide.classList.remove('mobile-menu__is-closed');
+  });
+
+
+  
+  // closing the menu
+  menuElement.addEventListener('click', event => {
+    menuElement.classList.add('mobile-menu__is-closed');
+    navButtonOpen.classList.add('mobile-menu__is-closed');
+    navButtonHide.classList.add('mobile-menu__is-closed');
+  });
+
+  navButtonHide.addEventListener('click', event => {
+    menuElement.classList.add('mobile-menu__is-closed');
+    navButtonOpen.classList.add('mobile-menu__is-closed');
+    navButtonHide.classList.add('mobile-menu__is-closed');
+  });
+
 }
